a15c2fece407e82e439aa464f7ce721575b6fe1e
Premsinghhhh <72703592+Premsinghhhh@users.noreply.github.com>
2023-02-09 14:22:34 +0000

    Update audioPlayer.js

--- src/audioPlayer.js
+++ src/audioPlayer.js
@@ -82,9 +82,3 @@ function changeNavigator() {
   }
 }  
 
-
-// Load the first track when the page loads
-window.onload = function() {
-  audioPlayer.src = playlist.firstElementChild.firstElementChild.getAttribute("href");
-  audioPlayer.load();
-}
